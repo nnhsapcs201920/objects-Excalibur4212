a99afada0eb504a7224e852125953ead4fcd0fab
Derek Zhang <dzhang@student.naperville203.org>
2019-08-20 09:30:21 -0500

    yrdhgfdhgfdhgfdhgf

---
+++ ClassNotes/HelloPrinter.java
@@ -0,0 +1,28 @@
+
+/**
+ * Write a description of class HelloPrinter here (e.g., helloPrinter).
+ * in general, every source file (e.g. Helloprinter.java) contains one class (e.g., HelloPrinter)
+ * @author dereickius zhanganius
+ * the source filename much match the class name
+ * by convention, class names start with an uppercase letter.
+ * @version 08/20/2019
+ */
+public class HelloPrinter
+{
+    /*
+     * a class contains methods.
+     * methods are similar to functions in python
+     * most java applications contain a class with a main method.
+     * the main method is executed when the application starts
+     */
+    public static void main (String[] args){
+        /*
+         * a method contains statements
+         * statements may invoke other methods (e.g. println)
+         * in general, methods are invoked on objects (e.g. System.out)
+         * when invoking a method, arguments are passed in parentheses (e.g., "Hello, World!")
+         */
+        //this is a single-line comment
+        System.out.println("hello world"); //comment at the end of a line
+    }
+}
