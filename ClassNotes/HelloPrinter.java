1556243f63724831fe27b53be79f07afe87c3740
Derek Zhang <dzhang@student.naperville203.org>
2019-08-21 10:54:48 -0500

    8/20/2019 work

--- ClassNotes/HelloPrinter.java
+++ ClassNotes/HelloPrinter.java
@@ -5,7 +5,7 @@
  * @author dereickius zhanganius
  * the source filename much match the class name
  * by convention, class names start with an uppercase letter.
- * @version 08/20/2019
+ * @version 08/21/2019
  */
 public class HelloPrinter
 {
@@ -24,5 +24,33 @@ public class HelloPrinter
          */
         //this is a single-line comment
         System.out.println("hello world"); //comment at the end of a line
+        /*
+         * Identifiers are any combination of: 
+         *      digits(but not as the first character)
+         *      dollar signs
+         * these are various identifiers:
+         */
+        int y, x2, x_y, x$;
+        
+        // this is not a valid identifier:
+        //int 2y;
+        /*
+         * identifiers are case sensitive, meaning case matters.
+         * 
+         * these are three different identifiers:
+         */
+        String box, BOX, bOx;
+        
+        /*
+         * By convention, constants are all uppercase.
+         *      Use an underscore to separate words.
+         */
+        final int SPEED_OF_LIGHT = 300000000;
+        
+        /*
+         * by convention, most other identifiers start with a lowercase letter. Subsequent words start with an uppercase letter. (CamalCase)
+         * 
+         */
+        String firstName;
     }
 }
