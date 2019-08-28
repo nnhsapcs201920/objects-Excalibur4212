5a2c30cfcf42d7d633225582d0843df6619643d0
Derek Zhang <dzhang@student.naperville203.org>
2019-08-28 10:25:19 -0500

    afds

---
+++ ClassNotes/NumberVariablesPrinter.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class NumberVariablesPrinter here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class NumberVariablesPrinter
+{
+        public static void main(String[] ajdsoifpo){
+            double n1 = 150;
+            double n2 = n1;
+            
+            n2 = n2*20;
+            
+            System.out.println(n1);
+            System.out.println(n2);
+        }
+}
